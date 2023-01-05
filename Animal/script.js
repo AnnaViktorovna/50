5bb4937645f62dbf375a15b0d960e2e69b68fdd0
AnnaViktorovna <anna.bazileeva@gmail.com>
2023-01-05 13:19:32 -0500

    Email

---
+++ Animal/script.js
@@ -0,0 +1,40 @@
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
+
+const sounds = ['cow', 'turkey', 'rooster','goat', 'horse','donkey',  'pig', 'sheep' ]
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
